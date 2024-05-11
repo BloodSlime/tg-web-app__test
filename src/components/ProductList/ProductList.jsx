13d3cbf78a7fb347c0a5214ee681ef7e71dbd7b5
pshishmarov <p.shishmarov@gmail.com>
2024-05-11 18:58:55 +0700

    items list, cart and total price

--- src/components/ProductList/ProductList.jsx
+++ src/components/ProductList/ProductList.jsx
@@ -1,10 +1,53 @@
-import React from 'react';
+import React, {useState} from 'react';
 import "./ProductList.css"
+import ProductItem from "../ProductItem/ProductItem";
+import {useTelegram} from "../../hooks/useTelegram";
+
+const products =[
+    {id: 1, title: 'Джинсы', price: 5000, description: 'Синего цвета, прямые'},
+    {id: 2, title: 'Куртка', price: 10000, description: 'Зеленого цвета, длинная'},
+    {id: 3, title: 'Кепка баленси', price: 20000, description: 'Черного цвета, ракнутая'}
+]
+
+const getTotalPrice = (items = [] ) => {
+    return items.reduce((acc, item) => {
+        return acc += item.price;
+    }, 0)
+}
 
 const ProductList = () => {
+    const [addedItems, setAddedItems] = useState([]);
+    const tg = useTelegram()
+    const onAdd = (product) => {
+        const alreadyAdded = addedItems.find(item => item.id === product.id);
+        let newItems = [];
+
+        if(alreadyAdded) {
+            newItems = addedItems.filter(item => item.id !== product.id)
+        } else {
+            newItems = [...addedItems, product]
+        }
+
+        setAddedItems(newItems)
+
+        if(newItems.length === 0) {
+            tg.MainButton.hide();
+        } else {
+            tg.MainButton.show();
+            tg.MainButton.setParams({
+                text: `Купить ${getTotalPrice(newItems)}`
+            })
+        }
+    }
     return (
-        <div>
-            ProductList
+        <div className={'list'}>
+            {product.map(item => {
+                <ProductItem
+                    product={item}
+                    onAdd={onAdd}
+                    className={'item'}
+                    />
+            })}
         </div>
     );
 };
